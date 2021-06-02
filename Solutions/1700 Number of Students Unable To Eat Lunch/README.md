81410622edda848a8ad8895c6529922eb7ff02e1
SidewaysFinch <souksamlanebrendan@gmail.com>
2021-06-01 21:11:42 -0600

    Added 724. Find Pivot Index. Cleaned up some parts of repo.

--- Solutions/1700 Number of Students Unable To Eat Lunch/README.md
+++ Solutions/1700 Number of Students Unable To Eat Lunch/README.md
@@ -1,6 +1,6 @@
 # 1700. Number of Students Unable to Eat Lunch
 
-## Link: https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
+Link: https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
 
 ## Brief
 
---
+++ Solutions/724 Find Pivot Index/724.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+     int pivotIndex(std::vector<int>& nums) {
+        //Start by assuming the index is 0
+        int left = 0;
+        int right = 0;
+        int index = 0;
+
+        //Right side will not ever include the first number
+        for (int i = 1; i < nums.size(); i++) {
+            right += nums[i];
+        }
+
+        /* Handle edge case behaviours:
+         * when size = 1 leetcode answer interprets [-1] and [1] as return 0
+         * otherwise check 0th index condition
+        */
+        if (nums.size() == 1 || (left == right)) {
+            return 0;
+        }
+
+        for (index = 1; index < nums.size(); index++) {
+            //Pivot a number and compare the new sums
+            left += nums[index-1];
+            right -= nums[index];
+            
+            if (left == right) {
+                return index;
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    std::vector<int> nums = {-1, 1};
+
+    Solution s;
+    return s.pivotIndex(nums);
+}
