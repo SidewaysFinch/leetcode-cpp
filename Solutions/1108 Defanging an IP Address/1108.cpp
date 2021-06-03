63f1655d2ec72408bd8a97e9d4baa1c84f464a31
SidewaysFinch <souksamlanebrendan@gmail.com>
2021-06-02 18:15:35 -0600

    Added comments to 1108 soln

--- Solutions/1108 Defanging an IP Address/1108.cpp
+++ Solutions/1108 Defanging an IP Address/1108.cpp
@@ -5,10 +5,14 @@ class Solution {
 public:
     std::string defangIPaddr(std::string address) {
         std::string defanged = "";
+
+        //Non-regex soln: step through every char
         for (size_t i = 0; i < address.length(); i++) {
+            //If we come across a period, copy over the defanged ver
             if (address[i] == '.') {
                 defanged.append("[.]");
             }
+            //Otherwise just copy over the char
             else {
                 defanged += address[i];
             }
